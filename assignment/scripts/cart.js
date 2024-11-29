a6ef14ef43bfb600678d5e3408454496bb9ab683
Alexander Ruziska <ziskapants@gmail.com>
2024-11-29 17:43:27 -0600

    Need to create 'empty' function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,13 +1,25 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const basket = ['ham', 'bacon'];
+function addItem(item){
+    basket.push(item);
+    return true;  
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(basket){
+    for (item of basket){
+        console.log(item);
+}
+}
 
 
-
-
-
-
-
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
 
 
 
