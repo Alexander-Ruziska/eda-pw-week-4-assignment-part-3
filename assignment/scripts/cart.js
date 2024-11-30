e60eab12a4126f9a3d80d2351b92a008d849821f
Alexander Ruziska <ziskapants@gmail.com>
2024-11-29 20:27:10 -0600

    stretch goals left

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,7 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-const basket = ['ham', 'bacon'];
+const basket = [];
 function addItem(item){
     basket.push(item);
     return true;  
@@ -10,15 +10,17 @@ console.log(`Basket is ${basket}`);
 console.log('Adding apples (expect true)', addItem('apples'));
 console.log(`Basket is now ${basket}`);
 
-function listItems(basket){
-    for (item of basket){
-        console.log(item);
+function listItems(){
+    for (i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
 }
 }
 
+function empty() {
+    basket.length = 0;
+}
 
-console.log(`Basket is ${basket}`);
-console.log('Adding apples (expect true)', addItem('apples'));
+empty();
 console.log(`Basket is now ${basket}`);
 
 
